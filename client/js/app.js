f71e29129e24bf27c1d441c961d37f0b9d5b0c79
luiz <munizluiz79@gmail.com>
2025-12-10 17:41:38 -0300

    subindo alterações

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,16 @@
-console.log("Frontend funcionando!");
+function login() {
+    const user = document.getElementById("userInput").value;
+    const pass = document.getElementById("passwordInput").value;
+
+    if (user === "" || pass === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    // Login provisório
+    if (user === "admin" && pass === "123") {
+        window.location.href = "dashboard.html"; // criar depois
+    } else {
+        alert("Usuário ou senha incorretos.");
+    }
+}
